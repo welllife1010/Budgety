66c91cf9c2714920ad0225901e970a5601f29033
Silvia Chen <welllife1010@gmail.com>
2018-07-02 12:38:28 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// BUDGET CONTROLLER
+/* --------------------------------------------------------------------------------------------
+		MODULES
+1. Important aspect of any robust application's architecture;
+2. Keep the units of code for a project both cleanly separated and organized;
+3. Encapsulated some data into privacy and expose other data publicly.
+
+So basically, modules allow us to break up our code into logical parts which are the modules,
+and then make them interact with one another.
+
+-------------------------------------------------------------------------------------------- */
+/* --------------------------------------------------------------------------------------------
+
+1. UI MODULE
+2. DATA MOFULE
+3. CONTROLLER MODULE - control the entire app and basically acting as a link between the other 
+two modules.
+
+-------------------------------------------------------------------------------------------- */
