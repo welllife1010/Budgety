c8a74ea2892f31b288df12471a4d86881bbe9895
Silvia Chen <welllife1010@gmail.com>
2018-07-05 15:57:38 -0700

    Updating the Percentage: UI Controller
    How to create our own forEach function but for nodeLists instead of arrays.

--- app.js
+++ app.js
@@ -189,7 +189,8 @@ var UIController = (function() {
 		incomeLabel: '.budget__income--value',
 		expensesLabel: '.budget__expenses--value',
 		percentageLabel: '.budget__expenses--percentage',
-		container: '.container'
+		container: '.container',
+		expensesPercLabel: '.item__percentage'
 	}
 
 	return {
@@ -243,6 +244,23 @@ var UIController = (function() {
 				document.querySelector(DOMstrings.percentageLabel).textContent = '---';
 			}
 		},
+		displayPercentages: function(percentages) {
+			var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+			var nodeListForEach = function(list, callback) {
+				for (var i = 0; i < list.length; i++) {
+					callback(list[i], i);
+				}
+			};
+
+			nodeListForEach(fields, function(current, index){
+				if (percentages[index] > 0) {
+					current.textContent = percentages[index] + '%';
+				} else {
+					current.textContent = '---';
+				}	
+			});
+		},
 		getDOMstrings: function() {
 			return DOMstrings;
 		}
@@ -285,7 +303,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 		// 2. Read percentages from the budget controller
 		var percentages = budgetCtrl.getPercentages();
 		// 3. Update the UI with the new percentages
-		console.log(percentages);
+		UICtrl.displayPercentages(percentages);
 	};
 
 	var ctrlAddItem = function() {
