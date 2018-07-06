8077bde43b1769eef527b4a737b461b740aa7675
Silvia Chen <welllife1010@gmail.com>
2018-07-05 22:59:54 -0700

    Finishing Touching: Improving the UX
    How and when to use 'change' events.

--- app.js
+++ app.js
@@ -227,6 +227,12 @@ var UIController = (function() {
 
 	};
 
+	var nodeListForEach = function(list, callback) {
+		for (var i = 0; i < list.length; i++) {
+			callback(list[i], i);
+		}
+	};
+
 	return {
 		getInput: function() {
 			return {
@@ -282,13 +288,6 @@ var UIController = (function() {
 		},
 		displayPercentages: function(percentages) {
 			var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
-
-			var nodeListForEach = function(list, callback) {
-				for (var i = 0; i < list.length; i++) {
-					callback(list[i], i);
-				}
-			};
-
 			nodeListForEach(fields, function(current, index){
 				if (percentages[index] > 0) {
 					current.textContent = percentages[index] + '%';
@@ -306,6 +305,13 @@ var UIController = (function() {
 			month = now.getMonth();
 			document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
 
+		},
+		changeType: function() {
+			var fields = document.querySelectorAll(DOMstrings.inputType + ',' + DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
+			nodeListForEach(fields, function(cur) {
+				cur.classList.toggle('red-focus');
+			});
+			document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
 		},
 		getDOMstrings: function() {
 			return DOMstrings;
@@ -332,6 +338,8 @@ var controller = (function(budgetCtrl, UICtrl) {
 			}
 		});// Event Delegation
 		document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+		document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changeType);
 	};
 
 	var updateBudget = function() {
